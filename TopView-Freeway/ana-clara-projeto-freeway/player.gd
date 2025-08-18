61714fff485bae036ad7da3613bd8715960cbfa3
anaclaramtn <anaclaramtn@gmail.com>
2025-08-18 14:14:14 -0300

    animacao concluida

--- TopView-Freeway/ana-clara-projeto-freeway/player.gd
+++ TopView-Freeway/ana-clara-projeto-freeway/player.gd
@@ -1,8 +1,10 @@
 extends Area2D
-
 var speed: float = 100.0;
 var screenSize: Vector2;
-var initialPosition: Vector2 = Vector2(640, 690); # posicao padrao para onde a galinha vai spawnar?
+var initialPosition: Vector2 = Vector2(640, 690);
+
+# @onready é uma forma fácil de pegar um nó filho.
+@onready var animacao = $animacao # esse é o nó "animacao" que é child do player criado (a galinha);
 
 func _ready() -> void:
 	screenSize = get_viewport_rect().size;
@@ -15,6 +17,15 @@ func _process(delta: float) -> void:
 		current_velocity.y -= 1;
 	if Input.is_action_pressed("ui_down"):
 		current_velocity.y += 1;
+	
+	
+	if current_velocity.y < 0:
+		animacao.play("cima");
+	elif current_velocity.y > 0:
+		animacao.play("baixo");
+	else:
+		if current_velocity.x == 0 || current_velocity.y == 0: 
+			animacao.stop()
 
 	if current_velocity.length() > 0:
 		current_velocity = current_velocity.normalized() * speed;
