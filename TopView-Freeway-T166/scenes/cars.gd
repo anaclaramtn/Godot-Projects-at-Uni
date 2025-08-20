3194eb9275b2db514bc5e89f79992ac3b1b3c5d3
Ana Clara <anaclaramtn@gmail.com>
2025-08-20 20:13:24 -0300

    added cars and HUD

---
+++ TopView-Freeway-T166/scenes/cars.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+
+func _ready() -> void:
+	var typeCars = $animation.sprite_frames.get_animation_names();
+	var car = typeCars[randi_range(0, typeCars.size() - 1)];
+	
+	$animation.animation = car;
