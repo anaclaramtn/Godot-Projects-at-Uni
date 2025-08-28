a4d6e61e8d1db766a698b2ffc0f35b243afb6409
anaclaramtn <anaclaramtn@gmail.com>
2025-08-28 14:25:16 -0300

    added my new game based on freeway

---
+++ TopView-Freeway-T166/freeway-modified-myversion/scenes/mobs.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+@onready var animation = $AnimatedSprite2D
+
+@export var mob_name = "orc-normal-run"
+
+
+func _physics_process(delta):
+	# Apenas move o mob para a direita (sentido positivo do eixo X)
+	velocity.x = speed
+	move_and_slide()
+	
+func _ready():
+	animation.play(mob_name)
