61633958743b117dae13cde2a178e4c2205ee25e
Ana Clara <anaclaramtn@gmail.com>
2025-08-28 20:57:49 -0300

    fixing

---
+++ TopView-Freeway-T166/freeway-mod-atualizado/Cenas/player.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+@export var speed: float = 150.0;
+var screen_size: Vector2;
+@export var initial_position: Vector2 = Vector2(600, 640);
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size;
+	position = initial_position;
+	
+func _process(delta):
+	var velocity = Vector2.ZERO;
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1;
+		$animation.play("cima");
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1;
+		$animation.play("baixo");
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1;
+		$animation.play("esquerda");
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1;
+		$animation.play("direita");
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed;
+	
+	position += velocity * delta;
+	position.y = clamp(position.y, 0.0,  screen_size.y);
+
+	if velocity.length() == 0:
+		$animation.stop();
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Terrain":
+		emit_signal("pontua")
+		position = initial_position
+	else:
+		position = initial_position
