7d40aa0ff2ac49a0a0df49ffab4138f0dc592409
Ana Clara <anaclaramtn@gmail.com>
2025-09-15 20:11:21 -0300

    more enemies

--- Mini-Mario-Demo-T166/Cenas/goomba.gd
+++ Mini-Mario-Demo-T166/Cenas/goomba.gd
@@ -7,3 +7,12 @@ func _die():
 	set_collision_layer_value(3, false)
 	set_collision_mask_value(1, false)
 	get_tree().create_timer(0,6).timeout.connect(queue_free)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Koopa && (area as Koopa).in_a_shell && (area as Koopa).h_speed != 0:
+		_die_from_hit()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
