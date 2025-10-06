f19284aca638838afd77390676dda576d482be7b
anaclaramtn <anaclaramtn@gmail.com>
2025-10-06 17:50:17 -0300

    changed the parallax

---
+++ Martooine-DEMO/ENEMIES/enemy_slime.gd
@@ -0,0 +1,97 @@
+extends CharacterBody2D
+
+var enemy_death_effect = preload("res://SCENES/enemy_death_effect.tscn")
+
+@export var patrol_points : Node
+@export var speed : int = 2000
+@export var wait_time : int = 2
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
+
+var gravity = 1000
+
+enum State { Idle, Walk }
+var current_state : State
+var direction : Vector2 = Vector2.LEFT
+var number_of_points : int
+var point_positions : Array [Vector2]
+var current_point : Vector2
+var current_point_position : int
+var can_walk : bool
+
+@export var health_amount : int = 3
+
+func _ready() -> void:
+	if patrol_points != null:
+		number_of_points = patrol_points.get_children().size()
+		for point in patrol_points.get_children():
+			point_positions.append(point.global_position)
+		current_point = point_positions[current_point_position]
+	else:
+		print("no patrol points")
+	
+	timer.wait_time = wait_time
+	
+	current_state = State.Idle
+
+
+func _physics_process(delta: float) -> void:
+	enemy_gravity(delta)
+	enemy_idle(delta)
+	enemy_walk(delta)
+	enemy_animation()
+	
+	move_and_slide()
+	
+
+func enemy_gravity(delta: float):
+	velocity.y += gravity * delta
+
+
+func enemy_idle(delta: float):
+	if !can_walk:
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		current_state = State.Idle
+
+func enemy_walk(delta : float):
+	if !can_walk:
+		return
+	
+	if abs(position.x - current_point.x) >  0.5:
+		velocity.x = direction.x * speed * delta
+		current_state = State.Walk
+	else:
+		current_point_position = (current_point_position + 1) % point_positions.size()
+		current_point = point_positions[current_point_position]
+
+	if current_point.x > position.x:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
+		
+	animated_sprite_2d.flip_h = direction.x > 0
+
+
+func _on_timer_timeout() -> void:
+	can_walk = true
+
+func enemy_animation():
+	if current_state == State.Idle && !can_walk:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Walk && can_walk:
+		animated_sprite_2d.play("walk")
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health_amount -= node.damage_amount
+		
+		print("health amount: ", health_amount)
+		
+		if health_amount <= 0:
+			var enemy_death_effect_instance = enemy_death_effect.instantiate() as Node2D
+			enemy_death_effect_instance.global_position = global_position
+			get_parent().add_child(enemy_death_effect_instance)
+			queue_free() 
---
+++ Martooine-DEMO/SCRIPTYS/player.gd
@@ -0,0 +1,113 @@
+extends CharacterBody2D
+
+var bullet = preload("res://SCENES/bullet.tscn")
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var muzzle: Marker2D = $Muzzle
+
+const gravity = 1000
+@export var speed : int = 1000
+@export var max_horizontal_speed: int = 250
+@export var slow_speed : int = 1700
+
+@export var jump = -300
+@export var jump_horizontal_speed : int = 150
+
+enum State {Idle, Run, Jump, Shoot}
+
+var current_state = State.Idle
+var muzzle_position : Vector2
+
+
+func _ready():
+	current_state = State.Idle
+	muzzle_position = muzzle_position 
+
+var last_direction = 1
+func _physics_process(delta):
+	player_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+	player_jump(delta)
+	
+	player_muzzle_position()
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		last_direction = direction
+
+	if Input.is_action_just_pressed("shoot"):
+		player_shoot(delta)
+	
+	move_and_slide()
+	
+	player_animation()
+	#print("State: ", State.keys()[current_state])
+	# VERIFICAR O ESTADO DO PLAYER ESTA COMENTADO NO MOMENTO
+
+func player_falling (delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+
+func player_idle(delta):
+	if is_on_floor() and current_state != State.Shoot:
+		current_state = State.Idle
+		
+#func player_idle(delta):
+	#if is_on_floor():
+		#current_state = State.Idle
+		## print("State: ", State.keys()[current_state])
+
+func player_run(delta):
+	if !is_on_floor():
+		return
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x += direction * speed * delta
+		velocity.x = clamp(velocity.x,-max_horizontal_speed,max_horizontal_speed)
+	else:
+		velocity.x = move_toward(velocity.x, 0 , slow_speed * delta)
+		
+	
+	if direction != 0:
+		current_state = State.Run
+		# print("State: ", State.keys()[current_state])
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func player_jump(delta):
+	# se a acao pressionada for pulo e o player estiver no chao
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump
+		current_state = State.Jump
+		
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * jump_horizontal_speed * delta
+		velocity.x = clamp(velocity.x,-jump_horizontal_speed,jump_horizontal_speed)
+
+
+func player_shoot(delta):
+	var bullet_instance = bullet.instantiate() as Node2D
+	bullet_instance.direction = last_direction
+	bullet_instance.global_position = muzzle.global_position
+	get_parent().add_child(bullet_instance)
+	current_state = State.Shoot
+
+func player_muzzle_position():
+	var direction = Input.get_axis("move_left","move_right")
+	
+	if last_direction > 0:
+		muzzle.position.x = abs(muzzle.position.x)
+	elif last_direction < 0:
+		muzzle.position.x = -abs(muzzle.position.x)
+
+func player_animation():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
+	elif current_state == State.Shoot:
+		animated_sprite_2d.play("shoot")
